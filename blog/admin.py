d9ec94252278455996dee497df151bd666dfb2a7
matveymex <mmat9@me.com>
2020-09-17 16:48:16 +0300

    add mnogo

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Category
+from .models import Category, Tag, Post, Comment
+
+
 admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
